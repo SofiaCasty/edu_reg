e83a8fd3dc832a6e9afbe2d19b71ee0d25b62972
sofia-romero-dev <amanda.romero@goes.gob.sv>
2026-03-25 13:35:48 -0600

    cambios bd

--- app/config.py
+++ app/config.py
@@ -12,16 +12,16 @@ class Settings(BaseSettings):
     environment: str = Field(default="development", alias="APP_ENV")
     secret_key: str = Field(default="change-me-in-production", alias="SECRET_KEY")
     database_url: str = Field(
-        default="mysql+pymysql://edu_user:goes-ia-apps$2026@/edu_reg?unix_socket=/cloudsql/PROJECT_ID:REGION:INSTANCE",
+        default="mysql+pymysql://edu_user:goes-ia-apps$2026@/edu_reg?unix_socket=/cloudsql/goes-ia-apps:us-central1:edu-reg-db",
         alias="DATABASE_URL",
     )
     session_cookie: str = "school_session"
     default_admin_email: str = "admin@antigravity.school"
-    default_admin_password: str = "Admin123!"
-    import_archive_path: str = Field(
-        default="/app/data/drive-download-20260317T201651Z-1-001.zip",
-        alias="IMPORT_ARCHIVE_PATH",
-    )
+    default_admin_password: str = "Admin#2026"
+    #import_archive_path: str = Field(
+    #    default="/app/data/drive-download-20260317T201651Z-1-001.zip",
+    #   alias="IMPORT_ARCHIVE_PATH",
+    #)
     app_host: str = Field(default="0.0.0.0", alias="APP_HOST")
     app_port: int = Field(default=8000, alias="APP_PORT")
 
--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ settings = get_settings()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    #Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
     db = SessionLocal()
     try:
         bootstrap_database(db)
